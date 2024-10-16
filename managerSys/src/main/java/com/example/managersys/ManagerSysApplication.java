e2a74622a14f65ea4ffaf11d7222ed4cce045ade
sky-in-shine <148601642+sky-in-shine@users.noreply.github.com>
2024-10-16 17:33:15 +0800

    Add files via upload

---
+++ managerSys/src/main/java/com/example/managersys/ManagerSysApplication.java
@@ -0,0 +1,13 @@
+package com.example.managersys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerSysApplication.class, args);
+    }
+
+}
